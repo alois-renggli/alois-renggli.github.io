c2c43f3bf7197209d6e8c09c3d8aa7da3a158522
aloisrenggli <alois.renggli@gmail.com>
2022-08-02 08:02:13 -0500

    Updated

--- js/index.js
+++ js/index.js
@@ -1,109 +1,7 @@
 "use strict";
-$("#home").click(function(){
-    $("main").children().remove();
-    $("main").append("<p>\n" +
-        "<b>OBJECTIVE:</b>  Seeking a position as a Junior Software Developer in the San Antonio area.\n" +
-        "</p>\n" +
-        "<p>\n" +
-        "<b>SUMMARY:</b>  Full-stack web developer with over 13 years of military service. Creative, versatile,\n" +
-        "and motivated professional with management experience ranging from small groups to\n" +
-        "organizations with over 100 employees. Technically-fluent problem-solver that is\n" +
-        "innovative and adaptable. Experience includes managing sensitive materials, administrative support,\n" +
-        "    recruiting, helicopter maintenance operations, airspace coordination for unmanned aviation systems, and mentoring employees.\n" +
-        "</p>\n" +
-        "<hr>\n" +
-        "<div class=\"row pl-5\">\n" +
-        "    <ul class=\"col\">\n" +
-        "    <li><b>Secret Security Clearance</b></li>\n" +
-        "<li>Independent</li>\n" +
-        "<li>Disciplined</li>\n" +
-        "</ul>\n" +
-        "<ul class=\"col\">\n" +
-        "    <li>Problem Solving</li>\n" +
-        "<li>Conflict Management</li>\n" +
-        "<li>Self-controlled</li>\n" +
-        "</ul>\n" +
-        "\n" +
-        "<ul class=\"col\">\n" +
-        "    <li>Microsoft Word, Excel, Powerpoint</li>\n" +
-        "<li>HTML, CSS, Git, jQuery, JavaScript</li>\n" +
-        "<li>Java, MySQL, Spring</li>\n" +
-        "</ul>\n" +
-        "</div>\n" +
-        "<hr> ");
+$("#home").click(function pageRedirect() {
+    window.location.href =  "/";
 });
-
-
-$("#history").click(function(){
-    $("main").children().remove();
-    $("main").append("\t<section>\n" +
-        "\t\t<p>\n" +
-        "\t\t\t<strong>PROFESSIONAL EXPERIENCE</strong>\n" +
-        "\t\t</p>\n" +
-        "\t\t<p class=\"mt-1 mb-0\">\n" +
-        "\t\t\t<b>Program Administrator - Special Operations Aviation Planner / September 2017 – July 2018</b>\n" +
-        "\t\t</p>\n" +
-        "\t\t<p class=\"mb-0\">\n" +
-        "\t\t\tU.S. Army, Fort Polk, LA\n" +
-        "\t\t</p>\n" +
-        "\t\t<ul class=\"pl-5\">\n" +
-        "\t\t\t<li>Expanded training area for Special Operations aviation by over 50% though coordination with the Federal Aviation Administration, Department of the Army Representative, Air Force Special Operations Command, Air Traffic Control representatives, and other government organizations.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t\t<li>Planned and coordinated aviation support (Unmanned Aircraft Systems, fixed wing, and rotary wing) for Special Operations Forces for 10 simulated combat rotations at the Joint Readiness Training Center.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t\t<li>Established and enforced standardized policies for Special Operations aviation requests and data collection in authorized operational areas.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t</ul>\n" +
-        "\n" +
-        "\t\t<p class=\"mt-1 mb-0\">\n" +
-        "\t\t\t<b>Operations Officer - Aviation Maintenance / June 2016 – August 2017</b>\n" +
-        "\t\t</p>\n" +
-        "\t\t<p class=\"mb-0\">\n" +
-        "\t\t\tU.S. Army, Fort Hood, TX\n" +
-        "\t\t</p>\n" +
-        "\t\t<ul class=\"pl-5\">\n" +
-        "\t\t\t<li>Successfully planned and executed a simulated combat rotation for my maintenance troop at the National\n" +
-        "\t\t\t\tTraining Center in 2017. My troop was assessed as one of the best aviation maintenance troops to complete a\n" +
-        "\t\t\t\trotation in over 2 years (approximately 20 rotations).\n" +
-        "\t\t\t</li>\n" +
-        "\t\t\t<li>Directed daily maintenance operations of 24 AH-64E helicopters and 8 RQ-7B Shadow Unmanned Aerial Systems valued in excess of $960 million. Exceeded the Army standard for operational readiness rate of aviation assets by over 10%.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t\t<li>Supervised an organization of 115 employees with tools and equipment valued in excess of $14 million.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t</ul>\n" +
-        "\n" +
-        "\t\t<p class=\"mt-1 mb-0\">\n" +
-        "\t\t\t<b>Program Administrator – Helicopter Operations / May 2010 – June 2016</b>\n" +
-        "\t\t</p>\n" +
-        "\t\t<p class=\"mb-0\">\n" +
-        "\t\t\tU.S. Army, Fort Drum, NY; Fort Rucker, AL; Fort Hood, TX; Afghanistan\n" +
-        "\t\t</p>\n" +
-        "\t\t<ul class=\"pl-5\">\n" +
-        "\t\t\t<li>Prepared my assigned personnel for a combat rotation to Afghanistan in 2013 through rigorous training. Completed a 9 month operation in Afghanistan with no combat injuries to personnel or loss of equipment.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t\t<li>Planned and managed daily operations for brigade and battalion sized organizations in excess of 4,000 Soldiers.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t\t<li>Prepared operational records and reports for GS-15 rank equivalent personnel.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t\t<li>Supervised and managed daily operations of 24 Soldiers in a platoon of 5 UH-60M helicopters and responsible for the accountability and maintenance of sensitive equipment valued in excess of $30 million.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t</ul>\n" +
-        "\n" +
-        "\t\t<p class=\"mt-1 mb-0\">\n" +
-        "\t\t\t<b>Infantryman / April 2001 - June 2006</b>\n" +
-        "\t\t</p>\n" +
-        "\t\t<p class=\"mb-0\">\n" +
-        "\t\t\tU.S. Army, Camp Smith, NY; Iraq\n" +
-        "\t\t</p>\n" +
-        "\t\t<ul class=\"pl-5\">\n" +
-        "\t\t\t<li>Conducted daily personal interactions with culturally diverse personnel while deployed to Iraq in 2004.</li>\n" +
-        "\t\t\t<li>Responsible for over $20,000 of professional equipment and sensitive items with no loss or damage over a 5 year period.\n" +
-        "\t\t\t</li>\n" +
-        "\t\t</ul>\n" +
-        "\t</section>\n" +
-        "\<hr>");
-});
-
 
 $("#project").click(function(){
     $("main").children().remove();
@@ -126,26 +24,6 @@ $("#project").click(function(){
 });
 
 
-// $("#references").click(function(){
-//     $("main").children().remove();
-//     $("main").append(" ");
-// });
-
-$("#resume").click(function(){
-    $("main").children().remove();
-    $("main").append("<div class=\"row\">" +
-        "<div class=\"col\"></div>" +
-        "<div class=\"col-6 blk-border mt-3\">\n" +
-        "    <h4 class=\"text-center\">Resume</h4>\n" +
-        "<ul>\n" +
-        "<li><a href=\"resume.html\" target=\"_blank\">&lt;html&gt; Version</a></li>\n" +
-    "</ul>\n" +
-        "</div>\n" +
-        "<div class=\"col\"></div>" +
-        "</div>\n" +
-        "<hr> ");
-});
-
 $("#contact").click(function(){
     $("main").children().remove();
     $("main").append("<div class=\"row mr-0\">\n" +
@@ -160,16 +38,10 @@ $("#contact").click(function(){
         "    Location: San Antonio, TX\n" +
         "</p>\n" +
         "<p class=\"m-0 \">\n" +
-        "    Resume: <a href=\"resume.html\" target=\"_blank\">&lt;html&gt;</a>\n" +
-        "</p>\n" +
-        "<p class=\"m-0 \">\n" +
         "<a href=\"https://www.linkedin.com/in/alois-renggli-9075378a\" target=\"_blank\" class=\"mr-2 align-middle\"><i class=\"fab fa-linkedin fa-2x linkedin mr-1\"></i> linkedin.com/in/alois-renggli-9075378a</a>\n" +
         "</p>\n" +
         "<p class=\"m-0 \">\n" +
         "<a href=\"https://github.com/alois-renggli\" target=\"_blank\" class=\"mr-2 align-middle\"><i class=\"fab fa-github fa-2x text-dark mr-1\"></i> github.com/alois-renggli</a>\n" +
-        "</p>\n" +
-        "<p class=\"m-0 \">\n" +
-        "<a href=\"mailto:alois.renggli@gmail.com\" class=\"mr-2\"><i class=\"far fa-envelope fa-2x text-dark mr-1\"></i> alois.renggli@gmail.com</a>\n" +
         "</p>\n" +
         "<div class=\"col\"></div>\n" +
         "</div>\n" +
